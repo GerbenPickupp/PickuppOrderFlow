78c4ae53f9d2e99aac619f26e016b8b932462800
Gerben <gerben.chen@pickupp.io>
2022-03-10 22:21:00 +0800

    'Order Flow Test Ver.0.0.1'

--- testrail.py
+++ testrail.py
@@ -1,36 +1,44 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 from testrail_api import TestRailAPI
+import configparser
+import json
 
-def testrail_connect():
-    api = TestRailAPI('https://pickupp.testrail.io/', 'gerben.chen@pickupp.io', 'Ss0128210')
-    return api
+testrail_config = configparser.ConfigParser()
+testrail_config.read('testrail.ini')
+api = TestRailAPI('https://pickupp.testrail.io/', 'gerben.chen@pickupp.io', 'Ss0128210#')
+#list(testrail_config[TestSuiteName]['case_ids'])
+def AddTestRunAndTest(TestSuiteName):
+    runId = api.runs.add_run(project_id=2,suite_id=1,name=TestSuiteName,include_all=False,case_ids=testrail_config[TestSuiteName]['case_ids'].split(','))
+    testIds = api.tests.get_tests(run_id=runId["id"]) 
+    return runId["id"],testIds
 
-def testrail_update_result(case_id,status_code,result):
-    api = testrail_connect()
-    api.results.add_result_for_case(
-            case_id=case_id,
-            status_id=status_code,
-            comment=result,
-            version="1"
-        )
-'''
-api = TestRailAPI('https://pickupp.testrail.io', 'gerben.chen@pickupp.io', 'Ss0128210#')
-my_test_run = api.runs.add_run(project_id=2,
-    suite_id=1,
-    name="Order Flow",
-    include_all=True,)
-print (my_test_run)
+def CheckTestIDWithCase(testIds,CaseName):
+    for i in range(len(testIds['tests'])):
+        if CaseName in testIds['tests'][i]['title']:
+            return testIds['tests'][i]['id'], testIds['tests'][i]['custom_steps_separated']
 
-api.results.add_result_for_case(
-    run_id=my_test_run["id"],
-    case_id=57,
-    status_id=1,
-    comment="Pass",
-    version="1"
-)
+def CountContentForResult(resultList,StepCounts):
+    result = []
+    for i in range(len(StepCounts)):
+        body = {
+            "content":"Step%s" % (i+1),
+            "actual":"%s" % resultList[((i*2)+1)],
+            "status_id":"%s" % resultList[((i*2)+2)]
+            }
+        result.append(body)
+    return result
 
-api.results.add_results_for_cases(
-    run_id = my_test_run['id'],
+def AddResultByStep(totalstatus,testIds,CaseName,resultList,runId):
+    #print (testId)
+    testId, StepCounts = CheckTestIDWithCase(testIds,CaseName)
+    result = CountContentForResult(resultList,StepCounts)
+    api.results.add_results(
+    run_id=runId,
     results = [
-        57,1,"Pass"]
-)
-'''
+        {"test_id": testId,
+            "status_id": totalstatus,
+            "custom_step_results": result
+        }]
+    )
+
